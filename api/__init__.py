20ade50744751a7fdc7609ae828413831e35b472
Geeks-Vegeta <shreyasmohite786@gmail.com>
2022-10-30 12:42:00 +0530

    initial commit

---
+++ api/__init__.py
@@ -0,0 +1,20 @@
+'''
+Api folder keeps track of all config
+Like Route, Resource, Database
+This folder is then exported to app.py
+where app.py will be able to run flask framework
+
+'''
+
+
+from flask import Flask
+from flask_restful import Api
+
+
+app = Flask(__name__)
+
+api = Api(app)
+
+from routes.home_route import HomeRoute
+
+api.add_resource(HomeRoute,"/")
